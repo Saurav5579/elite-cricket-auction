8d7f555b840fdaa89ea5bcd943386213e19bde1e
Saurav Kumar <sauravrathore25102002@gmail.com>
2026-03-07 10:39:19 +0530

    fix render allowed hosts

--- auction_project/settings.py
+++ auction_project/settings.py
@@ -18,7 +18,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "elitecricket.co.in",
-    "www.elitecricket.co.in"
+    "www.elitecricket.co.in",
+    "elite-cricket-auction.onrender.com"
 ]
 
 
