679b3810dd342c9d31fbd01d0f10fc98d9c864bc
Saurav Kumar <sauravrathore25102002@gmail.com>
2026-03-07 10:47:31 +0530

    fix media files render

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path("", include("players.urls")),
 ]
 
-# ✅ Media files serve in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
