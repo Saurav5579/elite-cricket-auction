b7b6845b0d3b234dc223ceed732b4b01652db5a1
Saurav Kumar <sauravrathore25102002@gmail.com>
2026-03-07 15:57:47 +0530

    Added sitemap

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -4,9 +4,25 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Sitemap imports
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+from players.models import Player
+
+
+# Sitemap configuration
+info_dict = {
+    "players": {
+        "queryset": Player.objects.all(),
+    }
+}
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("players.urls")),
+
+    # Sitemap URL
+    path("sitemap.xml", sitemap, {"sitemaps": info_dict}, name="sitemap"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
