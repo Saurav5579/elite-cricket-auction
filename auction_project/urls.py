44ee2566821344935c767d6111277ec9890172b8
Saurav Kumar <sauravrathore25102002@gmail.com>
2026-03-07 17:01:33 +0530

    fixed sitemap error

--- auction_project/urls.py
+++ auction_project/urls.py
@@ -4,25 +4,25 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Sitemap imports
+# sitemap imports
 from django.contrib.sitemaps.views import sitemap
 from django.contrib.sitemaps import GenericSitemap
 from players.models import Player
 
 
-# Sitemap configuration
-info_dict = {
-    "players": {
+# correct sitemap config
+sitemaps = {
+    "players": GenericSitemap({
         "queryset": Player.objects.all(),
-    }
+    }, priority=0.6)
 }
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("players.urls")),
 
-    # Sitemap URL
-    path("sitemap.xml", sitemap, {"sitemaps": info_dict}, name="sitemap"),
+    # sitemap url
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
