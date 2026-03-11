ebc9de613fa3de377f0e528bb60ecea0256fb45a
Saurav Kumar <sauravrathore25102002@gmail.com>
2026-03-11 12:26:19 +0530

    PostgreSQL setup

--- auction_project/settings.py
+++ auction_project/settings.py
@@ -171,3 +171,9 @@ EMAIL_HOST_PASSWORD = "hkauftdymjnmtrvv"
 DEFAULT_FROM_EMAIL = "Elite Cricket Auction <yourgmail@gmail.com>"
 ADMIN_EMAIL = "ar783524@gmail.com"
 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
